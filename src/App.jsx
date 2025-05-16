d2242de9603cd9b584e002faf89daf3838266bde
Taemin1 <rlaxoals132@naver.com>
2025-05-17 02:46:57 +0900

    refactor: 전체 레이아웃 구조 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,45 @@
-import { Routes, Route } from 'react-router-dom';
-import GlobalStyle from './styles/GlobalStyle';
-import Header from './assets/CommonComponents/Header/Header';
-import Navbar from './assets/CommonComponents/Navbar/Navbar';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import Home from './pages/Home/Home';
-import Profile from './pages/Profile/Profile';
-import Challenge from './pages/Challenge/Challenge';
-import FinDetail from './pages/FinDetail/FinDetail';
-import ChallengeDetail from './pages/ChallengeDetail/ChallengeDetail';
-import GroupDetail from './pages/GroupDetail/GroupDetail';
-import NoContents from '../src/assets/CommonComponents/NoContents/NoContents';
+import { Routes, Route } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
+import { LayoutWrapper } from "./LayoutWrapper";
+import Header from "./assets/CommonComponents/Header/Header";
+import Navbar from "./assets/CommonComponents/Navbar/Navbar";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import Challenge from "./pages/Challenge/Challenge";
+import FinDetail from "./pages/FinDetail/FinDetail";
+import ChallengeDetail from "./pages/ChallengeDetail/ChallengeDetail";
+import GroupDetail from "./pages/GroupDetail/GroupDetail";
+import NoContents from "./assets/CommonComponents/NoContents/NoContents";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Login />} />
 
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/home/fin/:finId" element={<FinDetail />} />
-        <Route
-          path="/home/challenge/:challengeId"
-          element={<ChallengeDetail />}
-        />
-        <Route
-          path="/home/challenge/:challengeId/:groupId"
-          element={<GroupDetail />}
-        />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/challenge" element={<Challenge />} />
-        <Route path="*" element={<NoContents />} />
-      </Routes>
-      <Navbar />
+      <LayoutWrapper>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/home/fin/:finId" element={<FinDetail />} />
+          <Route
+            path="/home/challenge/:challengeId"
+            element={<ChallengeDetail />}
+          />
+          <Route
+            path="/home/challenge/:challengeId/:groupId"
+            element={<GroupDetail />}
+          />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/challenge" element={<Challenge />} />
+          <Route path="*" element={<NoContents />} />
+        </Routes>
+
+        <Navbar />
+      </LayoutWrapper>
     </>
   );
 }
--- src/assets/CommonComponents/Header/Header.style.js
+++ src/assets/CommonComponents/Header/Header.style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
   margin-left: 1.625rem; /* 26px */
